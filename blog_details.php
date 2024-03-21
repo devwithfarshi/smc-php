3b6ccdf6fd082f6fefc16c9d7b275159ac68fc1f
MD Salman Farshi <devwithfarshi@gmail.com>
2024-03-21 17:22:36 +0600

    single blog page donw

--- blog_details.php
+++ blog_details.php
@@ -9,10 +9,48 @@
 
         </figure>
         <div class="blog_details_author_info">
-
+            <p>written by : <span class="blog_details_author_info_autor_name">Thomas Paul</span> </p>
+            <p>published on : <span class="blog_details_author_info_published_date">9. 6. 2021</span> </p>
         </div>
 
-
+        <h2 class="blog_details_title">Figma?, Sketch, XD or Lunacy? Who rules?</h2>
+        <p class="blog_details_desc">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi dicta, id distinctio
+            amet nemo, itaque a harum
+            tempore deserunt iure quis quibusdam. Accusamus soluta modi, eum commodi delectus architecto. Eius velit
+            voluptates reprehenderit inventore, reiciendis minus magnam delectus voluptate exercitationem quos assumenda
+            quis ut voluptatem optio est a similique accusamus consectetur repellendus! Laboriosam aperiam modi
+            laudantium perspiciatis cumque maxime et! Id minima deserunt placeat pariatur magnam vel sapiente incidunt
+            vero, molestiae odio enim tenetur quod assumenda asperiores quaerat aut, consectetur hic magni eum
+            consequatur delectus architecto dolor qui repellat? Necessitatibus a perspiciatis reiciendis blanditiis,
+            illo accusamus fugit eligendi voluptate eum esse dolore! Officiis, laudantium ab ipsam modi nemo asperiores,
+            odit illo fugit perspiciatis ex quidem aliquid, est exercitationem. In molestiae illum consequuntur et. Ut
+            delectus repellendus, temporibus facilis iure adipisci necessitatibus minima quasi officia explicabo!
+            Aliquid, minus atque. Unde libero, esse laborum maiores recusandae dolore deleniti tenetur, quo ad error
+            quae, aut consequatur cupiditate tempore repudiandae obcaecati praesentium placeat in nostrum fuga? Delectus
+            quas voluptatum laboriosam dolor natus quibusdam beatae! Recusandae culpa, maxime tenetur vero iste earum
+            quo debitis perferendis est sapiente aliquid dolorum! Minima sed tenetur voluptatum quia veniam, officiis
+            itaque nam deleniti laudantium fugiat id ut cupiditate facilis alias nulla velit doloremque et impedit sit
+            quo? Quis aliquid dolorem vel non commodi sunt, culpa tempora at facere deleniti suscipit labore, saepe
+            consectetur iure assumenda ex voluptate dignissimos rerum ea natus earum unde eligendi. Eligendi cumque iste
+            eaque aperiam nihil, temporibus repellat tempore, quasi accusamus, reprehenderit omnis! Est accusamus
+            pariatur suscipit quo nam quaerat officiis quibusdam doloremque maiores fuga tenetur magni, cumque sequi id
+            quod aliquam magnam. Nam nulla laborum nostrum, aliquam reprehenderit nemo consectetur pariatur iusto
+            voluptates eos. Fuga molestiae nemo perferendis totam hic impedit voluptas pariatur provident quidem
+            temporibus cum possimus autem ex, consequatur ratione doloremque libero, dolore vero voluptates dolorum
+            deleniti. Molestiae, enim mollitia recusandae impedit, consequatur soluta, ducimus quasi aut quod odio ab
+            vero quae. Officia, error nemo quas officiis atque explicabo, eius doloremque libero voluptates repellendus
+            est quae vel? Soluta officia nam maiores veritatis a aspernatur veniam sequi, enim blanditiis odit eveniet
+            iste dicta dolorem quos nostrum laborum recusandae quidem omnis fugit eos accusantium odio temporibus.
+            Culpa, vero! Unde, obcaecati quos? Impedit vitae similique suscipit debitis, distinctio libero quibusdam
+            expedita a ad veniam ipsa minus amet quas excepturi et sint, repudiandae est quaerat eveniet magni ab sit
+            laudantium nesciunt. Nam est non dolores, temporibus illum commodi quo quaerat modi at perferendis nobis a
+            itaque excepturi iure ducimus maiores inventore soluta ab vitae id voluptate distinctio consequatur
+            voluptates magnam. Modi incidunt fugiat maxime, magni eos mollitia voluptas fuga eum inventore. Voluptatem
+            aperiam officiis ipsa iusto totam consequatur deleniti cum doloribus facilis, incidunt quas, tempora sed
+            eligendi. Saepe, accusamus libero. Quis ab, inventore minus doloremque nisi possimus quod? Nemo vel mollitia
+            ratione eum fuga consequatur? Consectetur consequatur at nostrum est quisquam! Exercitationem ut, sed
+            delectus molestias tenetur, cupiditate ducimus quaerat dolore eius voluptatum voluptatem fugiat. Voluptatum
+            ex, quaerat ipsa sit placeat vitae. Iste dignissimos dolorum molestiae!</p>
     </section>
 </div>
 
